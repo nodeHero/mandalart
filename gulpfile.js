0a556b01d7f887a2fcd9955c72818502dd37c7d2
BK Choi <qriosity.bk@gmail.com>
2016-07-20 09:11:49 +0900

    Let gulp merge (#27)
    * css, js merge #7
    - 폴더구조 변경
      - app/js/lib : 외부 js라이브러리
      - app/js/plugin : 외부 js플러그인
      - app/js/sources : 내부 js소스
      - app/less : 내부 less소스
    - js,css merge 구현
      - modules.js.json, modules.less.json파일은 합칠 파일목록과 결과파일이름을 명시
      - 합칠때 header, footer를 붙일수 있게 해둠. header에는 프로젝트,파일,작성자의 정보가 표시됨
      - gulp task 추가
        - clean : 기존에 합쳐진 파일삭제
        - jsmerge : js파일을 위의 명세에 따라 합친 후 public/js로 이동
        - lessmerge : less파일을 위의 명세에 따라 합친 후 public/css로 이동
        - less : public/css의 모든 less파일을 css로 변경한 후 less파일 삭제
        - watch : less, js 소스파일이 변경되면 자동으로 다시 합침
        - livereload : js, less, ejs 소스파일이 변경되면 서버를 내렸다 올림
           - chrome-extension livereload를 설치하면 새로고침할 필요가 없음(자동으로 해줌. 크롬 짱짱)
    - 각 ejs파일의 js, css, ejs 참조부분 수정
    - blank.gif, ie9.js 삭제: 영웅이가 불필요하다 함
    - .gitignore에 public/js, public/css 폴더 추가
    
    * Update README.md
    
    * css, js merge #7
    - gulp task 변경
      - jsmerge : 기존동작 + livereload 추가
      - less : 기존동작 + livereload 추가
      - watch : 머지완료시 livereload하는 걸로 변경하기 위해 여기 있는 livereload 제거
      - livereload -> develop
        - nodemon 설정 변경 : 머지결과폴더는 무시하고 다른 controller 등 서버로직js가 변하면 서버재시작
    - mandalart.js 삭제

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,100 @@
+const appname = "mandalart";
+
 var gulp = require('gulp'),
   nodemon = require('gulp-nodemon'),
   plumber = require('gulp-plumber'),
   livereload = require('gulp-livereload'),
-  less = require('gulp-less');
+  less = require('gulp-less'),
+  runsync = require('run-sequence');
 
-gulp.task('less', function () {
-  gulp.src('./app/less/*.less')
-    .pipe(plumber())
-    .pipe(less())
-    .pipe(gulp.dest('./public/css'))
+var pkg = require('./package.json'),
+  fpath = {
+    'jssrc': './app/js',
+    'jsdist': './public/js',
+    'lesssrc': './app/less',
+    'lessdist': './public/css'
+  };
+
+gulp.task('clean', function(cb) {
+  var del = require('del');
+
+  return del([
+    fpath.jsdist +'/*.js', // public/js/*.js
+    fpath.lessdist +'/*.css', // public/css/*.js
+  ], cb);
+});
+
+gulp.task('jsmerge', function() {
+  var builder = require('gulp-module-builder'),
+    header = require('gulp-header'),
+    footer = require('gulp-footer'),
+    trailer = require('./gulp/trailer'),
+    dateformat = require('dateformat'),
+    now = new Date(),
+    headerOpt = {
+      pkg: pkg,
+      date: {
+        now: dateformat(now, 'yyyy-mm-dd HH:MM:ss'),
+        year: dateformat(now, 'yyyy'),
+      }
+    };
+
+  return gulp.src(fpath.jssrc +'/modules.js.json')
+    .pipe(builder())
+    .pipe(header(trailer.header, headerOpt))
+    .pipe(footer(trailer.footer))
+    .pipe(gulp.dest(fpath.jsdist))
     .pipe(livereload());
 });
 
-gulp.task('watch', function() {
-  gulp.watch('./app/less/*.less', ['less']);
+gulp.task('lessmerge', function() {
+  var builder = require('gulp-module-builder'),
+    header = require('gulp-header'),
+    footer = require('gulp-footer'),
+    trailer = require('./gulp/trailer'),
+    dateformat = require('dateformat'),
+    now = new Date(),
+    headerOpt = {
+      pkg: pkg,
+      date: {
+        now: dateformat(now, 'yyyy-mm-dd HH:MM:ss'),
+        year: dateformat(now, 'yyyy'),
+      }
+    };
+
+  return gulp.src(fpath.lesssrc +'/modules.less.json')
+    .pipe(builder({ext:'less'}))
+    .pipe(header(trailer.header, headerOpt))
+    .pipe(footer(trailer.footer))
+    .pipe(gulp.dest(fpath.lessdist));
+});
+
+gulp.task('less', function () {
+  var del = require('del');
+
+  gulp.src(fpath.lessdist + '/*.less')
+    .pipe(less())
+    .pipe(gulp.dest(fpath.lessdist))
+    .pipe(livereload());
+
+  del(fpath.lessdist + '/*.less');
+});
+
+gulp.task('watch', function () {
+  gulp.watch(fpath.lesssrc + '/**/*.less', function() {
+    runsync('lessmerge', 'less');
+  });
+  gulp.watch('./app/js/**/*.js', function() {
+    runsync('jsmerge');
+  });
 });
 
 gulp.task('develop', function () {
   livereload.listen();
   nodemon({
     script: 'app.js',
-    ext: 'js coffee ejs',
+    ext: 'ejs js',
+    ignore: ['app/js/', 'public/js/'],
     stdout: false
   }).on('readable', function () {
     this.stdout.on('data', function (chunk) {
@@ -33,8 +107,27 @@ gulp.task('develop', function () {
   });
 });
 
-gulp.task('default', [
-  'less',
-  'develop',
-  'watch'
-]);
+gulp.task('uglify', function() {
+  var uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
+
+  return gulp.src(fpath.jsdist +'/*.js')
+    .pipe(uglify({
+      preserveComments: 'some',
+      output: {
+        beautify: false,
+        source_map: null
+      },
+      compress: { warnings: false },
+      mangle: {}
+    }))
+    .pipe(gulp.dest(fpath.jsdist));
+});
+
+gulp.task('default', function(done) {
+  runsync('clean', 'lessmerge', 'less', 'jsmerge', 'uglify', done);
+});
+
+gulp.task('local', function(done) {
+  runsync('clean', 'lessmerge', 'less', 'jsmerge', 'develop', 'watch', done);
+});
